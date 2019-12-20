a2e6a74ed2d53d57742a84b8bba292ec872bbb11
kangah-codes <Joshua Akangah>
2019-12-20 21:55:02 +0000

    Graphing set

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ __author__ = "Joshua Akangah"
 
 import guizero
 from guizero import Window
-import matplotlib as plot
+import matplotlib.pyplot as plot
 import browserhistory as bh
 
 box = None
@@ -55,29 +55,57 @@ def getHistory(browser):
 
 def Nmaxelements(list1, N): 
 	final_list = [] 
-
+	urls = []
 	for i in range(0, N):  
 		max1 = 0
 		for j in range(len(list1)):  
-			if int(list1[j][0]) > max1: 
-				max1 = list1[j]
-		  
-		list1.remove(max1)
-		final_list.append(max1) 
+			final_list.append([list1[j][0], list1[j][1]])
 
-	print(final_list)
+	items = []
+	final_list.sort()
+	for i in final_list:
+		if i not in items:
+			items.append(i)
+	items.sort(reverse=True)
+	return items[0:10]
 
 def plotData(browser):
 	final_items = []
 	final_item = []
 
-	for i in parse_hist[0:5]:
+	parse_hist.sort(reverse=True)
+
+	for i in parse_hist[0:500]:
 		if i not in final_items:
-			final_items.append([parse_hist[0:5].count(i), i])
+			final_items.append([parse_hist[0:500].count(i), i])
 	for i in final_items:
 		if i not in final_item:
 			final_item.append(i)
-	Nmaxelements(final_items[0:10], 10)
+
+	dict_of_vals = { i[1] : i[0] for i in Nmaxelements(final_item, 10) }
+
+	names = list(dict_of_vals.keys())
+	names1 = []
+	# bad code
+	for i in names:
+		names1.append(i.split('.com'))
+	vals = list(dict_of_vals.values())
+
+	plot.style.use('ggplot')
+
+	x = names
+	y = vals
+
+	x_pos = [i for i, _ in enumerate(x)]
+
+	plot.bar(x_pos, y, color='green')
+	plot.xlabel("Websites")
+	plot.ylabel("Visits")
+	plot.title("Most visited sites in browser history")
+
+	plot.xticks(x_pos, names)
+
+	plot.show()
 
 if __name__ == '__main__':	
 	main()
