288d672dd5126a62190d9dda10fa0ce299144bde
kangah-codes <Joshua Akangah>
2019-12-20 20:17:17 +0000

    Added history fetching

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 __author__ = "Joshua Akangah"
 
 import guizero
+from guizero import Window
 import matplotlib as plot
 import browserhistory as bh
 
 box = None
+history = None
+labels = None
 
 app = guizero.App(title="Vistory", width=500, height=600)
+hist_window = Window(app, title="History")
+hist_window.hide()
 
 def main():
 	guizero.Text(app, text=" ")
@@ -18,12 +23,18 @@ def main():
 	guizero.PushButton(box, text="Read HIstory from Browsers", command=findHistory)
 
 def findHistory():
+	global history
 	history = bh.get_browserhistory()
 	guizero.Text(box, text="Available browsers")
 	guizero.Text(box, text=" ")
 	for i in history.keys():
 		guizero.Text(box, text=" ")
-		guizero.PushButton(box, text=f"Read data from {i}")
+		guizero.PushButton(box, text=f"Read data from {i}", command=readData, args=[i])
+
+def getHistory(browser):
+	hist = []
+	hist.append(f"{_[0]}//{_[2]}" for _ in history[browser].split('/'))
+	print(hist)
 
 
 if __name__ == '__main__':
