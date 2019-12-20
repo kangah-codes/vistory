e4758e91699229b3facf6a6610effc1d23be9f59
kangah-codes <Joshua Akangah>
2019-12-20 21:07:23 +0000

    More improvements

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import browserhistory as bh
 
 box = None
 history = None
-labels = None
+hist = None
+times = None
+parse_hist = None
 
 app = guizero.App(title="Vistory", width=500, height=600)
 hist_window = Window(app, title="History")
@@ -20,7 +22,7 @@ def main():
 
 	global box
 	box = guizero.Box(app, width="fill")
-	guizero.PushButton(box, text="Read HIstory from Browsers", command=findHistory)
+	guizero.PushButton(box, text="Check browsers", command=findHistory)
 
 def findHistory():
 	global history
@@ -31,8 +33,13 @@ def findHistory():
 		guizero.PushButton(box, text=f"Read data from {i}", command=getHistory, args=[i])
 
 def getHistory(browser):
+	global hist
+	global times
+	global parse_hist
 	hist = []
+	times = []
 	for i in history[browser]:
+		times.append(i[-1])
 		hist.append(i[0].split('/'))
 
 	parse_hist = []
@@ -42,7 +49,35 @@ def getHistory(browser):
 	hist_window.show()
 	guizero.Text(hist_window, text=f"History for {browser}")
 	guizero.Text(hist_window, text=f" ")
-	guizero.ListBox(hist_window, items=parse_hist, width="fill")
+	guizero.Text(hist_window, text="If no history appears, try closing the browser and restart the app.", align="bottom")
+	guizero.ListBox(hist_window, items=list(zip(times, parse_hist)), width="fill", height="fill", scrollbar=True)
+	guizero.PushButton(hist_window, text="Plot data", command=plotData, args=[browser], align="bottom")
+
+def Nmaxelements(list1, N): 
+	final_list = [] 
+
+	for i in range(0, N):  
+		max1 = 0
+		for j in range(len(list1)):  
+			if int(list1[j][0]) > max1: 
+				max1 = list1[j]
+		  
+		list1.remove(max1)
+		final_list.append(max1) 
+
+	print(final_list)
+
+def plotData(browser):
+	final_items = []
+	final_item = []
+
+	for i in parse_hist[0:5]:
+		if i not in final_items:
+			final_items.append([parse_hist[0:5].count(i), i])
+	for i in final_items:
+		if i not in final_item:
+			final_item.append(i)
+	Nmaxelements(final_items[0:10], 10)
 
 if __name__ == '__main__':	
 	main()
